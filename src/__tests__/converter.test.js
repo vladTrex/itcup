4c8ff92c6ccf2f31ffe64b3119b8fc1f26d4d784
Vladyslav Tertyshnyi <vladislafus@gmail.com>
2019-06-02 22:25:48 +0300

    add mocha chai

---
+++ src/__tests__/converter.test.js
@@ -0,0 +1,9 @@
+import {expect, should} from 'chai';
+
+describe('User Schema', () => {
+    it('user creation', () => {
+        // const url = 'http://localhost:3000/auth/register';
+        const add = (a,b) => a+b;
+        expect(add(2,2)).to.equal(4);
+    });
+});
